9455264723bc09edde4ff8362ba02cc06f923141
Miikka <Oscar11212@outlook.com>
2024-02-01 15:04:40 -0500

    added files

---
+++ LAB02/program13.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+
+double speed(double temp){
+    double a = 1086 * sqrt((5 * temp + 297)/247);
+
+    a = 1.09728*a;
+
+    return a;
+} 
+
+
+
+int main(void){
+    double temp, result;
+    printf("please type a number for temperature");
+    scanf("%lf", &temp);
+
+    temp = temp*9/5 +32;
+    result = speed(temp);
+
+    printf("the speed of sound at %lf farenheit is: %lf", temp, result);
+
+
+
+
+    
+    return 0;
+}

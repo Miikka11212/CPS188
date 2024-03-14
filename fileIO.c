98cf38df5269563647f9dce907144e2d360ee17c
Miikka <Oscar11212@outlook.com>
2024-03-14 14:02:13 -0400

    1

--- fileIO.c
+++ fileIO.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-FILE *in;
+#include <stdlib.h>
 
-int main(){
-    in = fopen("file.txt", "r");
-    
+int main(void) {
+    char buffer[1000];
+
+    printf("Hello, world!\n");
+
+    FILE *transactions_file = fopen("transactions.csv", "r");
+    if (transactions_file == NULL) {
+        printf("Error: could not open file\n");
+        exit(-1);
+    }
+
+    fgets(buffer, sizeof(buffer), transactions_file);
+    printf("%s\n", buffer);
+
+    fclose(transactions_file);
+
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-#include <math.h>
 
-int main(){
-    int a, b;
-    float c, d;
-    a = 0, b = 0, c = 0, d = 0;
+int main()
+{
+   FILE *fp;
+   char str[60];
 
-    printf("Input an integer for one side of the triangle: \n");
-    scanf("%d", &a);
-
-    printf("Input another integer for the other side of the triangle: \n");
-    scanf("%d", &b);
-
-    c = sqrt(a*a + b*b);
-    d = a + b + c;
-//    printf("%0.2f", c);
-
-    printf("The hypotenus of this triangle is: %0.2f \n", c);
-    printf("The perimeter of this triangle is: %0.2f", d);
-
-
+   /* 打开用于读取的文件 */
+   fp = fopen("file.txt" , "r");
+   if(fp == NULL) {
+      perror("打开文件时发生错误");
+      return(-1);
+   }
+   if( fgets (str, 60, fp)!=NULL ) {
+      /* 向标准输出 stdout 写入内容 */
+      puts(str);
+   }
+   fclose(fp);
+   
+   return(0);
 }
